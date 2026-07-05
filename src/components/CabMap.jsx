af7a6375c2462fff115c5b22e1173ab0e901cfff
ksubram2 <karthikeyan180199@gmail.com>
2026-07-05 10:02:08 +0530

    style: change map tiles to CartoDB Voyager for clean white, blue, and green color scheme

--- src/components/CabMap.jsx
+++ src/components/CabMap.jsx
@@ -166,10 +166,10 @@ export default function CabMap({ pickup, drop, driver }) {
   return (
     <div className="map-container" style={{ height: '100%', borderRadius: 'inherit', border: 'none', boxShadow: 'none' }}>
       <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={true}>
-        {/* Standard OpenStreetMap tiles with natural colors (blue water, green parks/trees) */}
+        {/* Voyager tiles with clean color scheme (white roads/land, blue water, green parks) */}
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
 
         <RecenterMap center={mapCenter} bounds={bounds} />
